bae16f25014c9c712b9d853e981f09a588c280eb
unknown <widisuryo11@gmail.com>
2025-10-12 12:39:02 +0700

    lagi commit

--- scrapmagang.py
+++ scrapmagang.py
@@ -48,7 +48,7 @@ def ambil_data_api():
         time.sleep(0.05)
 
     progress.empty()
-    status.text(f"✅ Selesai! Total data diperoleh: {len(all_data):,}")
+    status.text(f"Total data diperoleh: {len(all_data):,}")
     return all_data
 
 
