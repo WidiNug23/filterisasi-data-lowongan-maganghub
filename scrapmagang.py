8e4e6cabb172b4858f709b0092a4d4fd33cfb5cd
unknown <widisuryo11@gmail.com>
2025-10-13 13:12:27 +0700

    lagi commit

--- scrapmagang.py
+++ scrapmagang.py
@@ -206,6 +206,10 @@ st.markdown(
     * data jenis instansi masih dalam tahap training dan pengembangan
     </p>
     """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
         """
     <p style='color:#AAAAAA; font-size:13px; font-style:italic; text-align:left; margin-top:-10px; margin-bottom:10px;'>
     * untuk mengurutkan, klik/tekan masing-masing judul kolom
