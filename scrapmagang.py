67ca554d30439cce44a857b4227884c4c70c270e
unknown <widisuryo11@gmail.com>
2025-11-13 20:22:52 +0700

    lagi commit

--- scrapmagang.py
+++ scrapmagang.py
@@ -15,7 +15,8 @@ BASE_URL = "https://maganghub.kemnaker.go.id/be/v1/api/list/vacancies-aktif"
 LIMIT = 1000
 MAKS_HALAMAN = 600
 MAKS_WORKER = 50   # lebih stabil dari 100
-RETRY_DELAY = 2   # agar 3 kolom pas
+RETRY_DELAY = 2 
+ITEMS_PER_PAGE = 18
 
 # === CSS Modern & Neon + sembunyikan navbar ===
 st.markdown("""
