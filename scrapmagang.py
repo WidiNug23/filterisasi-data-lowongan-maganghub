fe20abbf6b6c8447fb4f8a2323fac0705a5e93ad
unknown <widisuryo11@gmail.com>
2025-11-14 07:16:12 +0700

    lagi commit

--- scrapmagang.py
+++ scrapmagang.py
@@ -13,7 +13,7 @@ st.title("Sistem Filterisasi Lowongan MagangHub")
 
 BASE_URL = "https://maganghub.kemnaker.go.id/be/v1/api/list/vacancies-aktif"
 LIMIT = 1100                   # Ubah ke nilai aman
-MAKS_HALAMAN = 550
+MAKS_HALAMAN = 600
 MAKS_WORKER = 20            # 100 terlalu besar, bikin server throttle
 REFRESH_INTERVAL = 3000
 ITEMS_PER_PAGE = 20 # agar 3 kolom pas
